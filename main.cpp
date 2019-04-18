f06af2c93bbc73551fc01b943a943359e2fa20f6
anrong_wn <anrong_wn@126.com>
2019-04-18 15:37:41 +0800

    增加 textcodec

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    //QTextCodec::setCodecForLocale(QTextCodec::codecForName("GB18030"));
+    QTextCodec *systemCodec = QTextCodec::codecForLocale();
+    qDebug() << "===systemCodec.naem = " << systemCodec->name();
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
